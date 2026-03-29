d4ad86ef62a50f93ac26d503ced21a80a7f2ee30
Nandakumar005 <nandakumardg8@gmail.com>
2026-03-29 16:39:47 +0530

    Add implementation of voting eligibility check based on age input

---
+++ vote.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class vote {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your age");
+        int a=sc.nextInt();
+        if(a>=18){
+        System.out.print("Your are eligble for vote");}
+        else
+        System.out.print("You are not eligible");
+           
+        
+
+        
+    }
+
+}

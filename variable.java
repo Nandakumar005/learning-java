f4dd59176423af44c296e622c98d1daa12dbd4aa
Nandakumar005 <nandakumardg8@gmail.com>
2026-03-29 11:48:06 +0530

    Add initial implementation of variable class with main method

---
+++ variable.java
@@ -0,0 +1,22 @@
+public class variable {
+    public static void main(String[]args){
+        int age=20;
+        Double height=5.7;
+        char grade='A';
+        boolean student=true;
+        String name="Nanda";
+        
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(student);
+        System.out.println(name);
+
+
+
+
+
+    }
+
+}
